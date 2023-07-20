d903234c4be6ff3b9d4778d1b06d60784b9886ab
RohanMane02 <126078260+RohanMane02@users.noreply.github.com>
2023-07-21 01:29:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+//document.querySelector("button").addEventListener("click", Handliclick);
+
+function togglePlay() {
+  var myAudio = document.getElementById("myaudio");
+  return myAudio.paused ? myAudio.play() : myAudio.pause();
+}
+function togglePlay1() {
+  var myAudio1 = document.getElementById("myaudio1");
+  return myAudio1.paused ? myAudio1.play() : myAudio1.pause();
+}
+function togglePlay2() {
+  var myAudio2 = document.getElementById("myaudio2");
+  return myAudio2.paused ? myAudio2.play() : myAudio2.pause();
+}
+function togglePlay3() {
+  var myAudio3 = document.getElementById("myaudio3");
+  return myAudio3.paused ? myAudio3.play() : myAudio3.pause();
+}
+function togglePlay4() {
+  var myAudio4 = document.getElementById("myaudio4");
+  return myAudio4.paused ? myAudio4.play() : myAudio4.pause();
+}
+function togglePlay5() {
+  var myAudio5 = document.getElementById("myaudio5");
+  return myAudio5.paused ? myAudio5.play() : myAudio5.pause();
+}
+function togglePlay6() {
+  var myAudio6 = document.getElementById("myaudio6");
+  return myAudio6.paused ? myAudio6.play() : myAudio6.pause();
+}
+
+function togglePlay7() {
+  var myAudio7 = document.getElementById("myaudio7");
+  return myAudio7.paused ? myAudio7.play() : myAudio7.pause();
+}
+
+function togglePlay8() {
+  var myAudio8 = document.getElementById("myaudio8");
+  return myAudio8.paused ? myAudio8.play() : myAudio8.pause();
+}
+
+function togglePlay9() {
+  var myAudio9 = document.getElementById("myaudio9");
+  return myAudio9.paused ? myAudio9.play() : myAudio9.pause();
+}
